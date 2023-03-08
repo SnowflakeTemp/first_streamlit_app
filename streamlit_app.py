c2313cad349c18b86b620ab54b5cdf170db556f8
SnowflakeTemp <127231337+SnowflakeTemp@users.noreply.github.com>
2023-03-08 10:19:51 -0600

    Update streamlit_app.py
    Added header for fruityvice and change response to show json data

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,5 +25,7 @@ st.dataframe(fruits_to_show)
 
 
 #display fruityvice api response
+st.header('Fruityvice Fruit Advice!')
+
 fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
