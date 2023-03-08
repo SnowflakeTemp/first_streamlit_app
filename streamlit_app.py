67f90fa9fc978c45cb91c5e163fd56ec9ee49883
SnowflakeTemp <127231337+SnowflakeTemp@users.noreply.github.com>
2023-03-08 10:42:42 -0600

    Update streamlit_app.py
    Created variable for input from the user and added variable to api request

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,10 @@ st.dataframe(fruits_to_show)
 
 #display fruityvice api response
 st.header('Fruityvice Fruit Advice!')
+fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
+st.write('The user entered', fruit_choice)
 
-fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
 #Take the json version of the response and normalize it
